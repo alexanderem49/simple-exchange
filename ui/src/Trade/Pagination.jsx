0ff2a54b75d60bc3eb0174408a121857564fc734
Grigori Grigoryan <grigori.grigoryan@bytepitch.com>
2023-11-13 12:25:50 +0400

    feature: changed pagination colors

--- ui/src/Trade/Pagination.jsx
+++ ui/src/Trade/Pagination.jsx
@@ -22,21 +22,29 @@ export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
 
   return (
     <div className="flex justify-center items-center space-x-2">
-      <button onClick={() => onPageChange(Math.max(1, currentPage - 1))} disabled={currentPage === 1}>
+      <button
+        onClick={() => onPageChange(Math.max(1, currentPage - 1))}
+        disabled={currentPage === 1}
+        className="text-gray-600"
+      >
         Previous
       </button>
-      {currentPage > 3 && <span>...</span>}
+      {currentPage > 3 && <span className="text-gray-600">...</span>}
       {pageNumbersToShow().map((number) => (
         <button
           key={number}
-          className={`${currentPage === number ? 'text-blue-600' : ''}`}
+          className={`${currentPage === number ? 'text-blue-600' : 'text-gray-600'}`}
           onClick={() => onPageChange(number)}
         >
           {number}
         </button>
       ))}
-      {currentPage < totalPages - 2 && <span>...</span>}
-      <button onClick={() => onPageChange(Math.min(totalPages, currentPage + 1))} disabled={currentPage === totalPages}>
+      {currentPage < totalPages - 2 && <span className="text-gray-600">...</span>}
+      <button
+        onClick={() => onPageChange(Math.min(totalPages, currentPage + 1))}
+        disabled={currentPage === totalPages}
+        className="text-gray-600"
+      >
         Next
       </button>
     </div>
